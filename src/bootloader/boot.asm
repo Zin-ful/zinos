8e0849131e7bdb89941a0e3a2e846a05778fe477
Zinful <zinful@tutamail.com>
2026-03-21 09:31:58 -0400

    reading from disk

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -75,19 +75,37 @@ main:
     mov si, msg_hello
     call puts
 
+    ; read something from floppy
+    mov [ebr_drive_number], dl
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7E00
+    call disk_read
+    cli
     hlt
     
 .halt:
     jmp .halt
 
+; error handling
+
+floppy_error:
+    mov si, msg_failed
+    call puts
+    jmp key_int_reboot
+
+key_int_reboot:
+    mov ah, 0
+    int 16h
+    jmp 0FFFFh:0
+
+.halt:
+    cli
+    hlt
 
 ; disk routines
 
-
-
-
 ; conv an lba addr to chs addr
-
 ;Parameters:
 ;   -ax: LBA address
 ;Returns:
@@ -119,7 +137,70 @@ lba_to_chs:
     pop ax
     ret
 
-msg_hello: db 'Hello world!', ENDL, 0
+;read disk sectors
+;Parameters:
+;   -ax: LBA address
+;   -cl: Num sectors to read
+;   -dl: Point to drive num
+;   es:bx: memory address of stored data
+
+disk_read:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    call lba_to_chs
+    pop ax
+
+    mov ah, 02h
+    mov di, 3
+
+.retry:
+    pusha ;save all registers
+    stc
+    int 13h
+    jnc .done
+    ;read failed
+    popa ;restore all registers
+    call disk_reset
+    int 13h
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    jmp floppy_error
+
+.done:
+    popa
+    
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    mov si, msg_success
+    call puts
+    ret
+
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
+    
+    
+msg_hello: db 'BOOTING ZINOS', ENDL, 0
+
+msg_success: db 'Read something from disk', ENDL, 0
+msg_failed: db 'ZINOS: Read disk error occured', ENDL, 0
+
+
 
 times 510-($-$$) db 0
 dw 0AA55h
